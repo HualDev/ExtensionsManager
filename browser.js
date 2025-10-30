b3b33fce97ce8d19f0d3b681277bc46302a8c71c
Hualdev <hucastillo@gmail.com>
2025-10-29 22:21:33 -0500

    Reading and showing information

--- browser.js
+++ browser.js
@@ -6,10 +6,25 @@ fetch('data.json')
         return response.json();
       })
       .then(data => {
-        // Mostrar datos en consola
         console.log(data[0].name);
-      console.log(data[1].name);})
+      console.log(data[0].logo);
+    
+      const lens = document.createElement('h1')
+      const img = document.createElement('img')
+      const p = document.createElement('p')
+      const div = document.createElement('div')
 
-const lens = document.createElement("div")
-lens.textContent = 0
-document.getElementById("list").appendChild(lens)
+      lens.textContent = data[0].name
+      img.src = data[0].logo
+      p.textContent = data[0].description
+      div.textContent = 'Remove'
+      
+
+
+      document.getElementById('image').appendChild(img)
+      document.getElementById("lens").appendChild(lens)
+      document.getElementById('lens').appendChild(p)
+      document.getElementById('action').appendChild(div)
+    
+    })
+
