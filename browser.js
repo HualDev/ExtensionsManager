9c757c61e7fb73b7601cacf277f427d0cd2b4df8
Hualdev <hucastillo@gmail.com>
2025-10-30 22:40:39 -0500

    Style to information, mobile version

--- browser.js
+++ browser.js
@@ -6,25 +6,25 @@ fetch('data.json')
         return response.json();
       })
       .then(data => {
-        console.log(data[0].name);
-      console.log(data[0].logo);
-    
-      const lens = document.createElement('h1')
-      const img = document.createElement('img')
-      const p = document.createElement('p')
-      const div = document.createElement('div')
-
-      lens.textContent = data[0].name
-      img.src = data[0].logo
-      p.textContent = data[0].description
-      div.textContent = 'Remove'
-      
-
-
-      document.getElementById('image').appendChild(img)
-      document.getElementById("lens").appendChild(lens)
-      document.getElementById('lens').appendChild(p)
-      document.getElementById('action').appendChild(div)
+        const list = document.getElementById('list')
+        data.forEach(element => {
+          const div = document.createElement('div');
+          div.classList.add('list')
+          div.innerHTML = `
+                            <div class= 'top'>
+                              <img src = ${element.logo}>
+                              <div class='description'>
+                                <h1> ${element.name}</h1>
+                                <p>${element.description}</p>
+                              </div>
+                            </div>
+                            <div class='action'>
+                              <div>Remove</div>
+                              <div> <div></div> </div>
+                            </div>
+                            `
+          list.appendChild(div)
+        });
     
     })
 
