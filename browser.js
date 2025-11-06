d850c9520ea1c1e6fe014cbdef74f1957657bb3a
Hualdev <hucastillo@gmail.com>
2025-11-06 08:17:28 -0500

    funtionalities of buttons

--- browser.js
+++ browser.js
@@ -31,10 +31,35 @@ fetch('data.json')
 
         document.getElementById("active").addEventListener("click",() =>{
           const list = document.getElementsByClassName("list")
-          for(let i =0; i<list.length;i++){
+          document.getElementById('active').classList.add('red-color')
+          document.getElementById('all').classList.remove('red-color')
+          document.getElementById('inactive').classList.remove('red-color')
+          for(let i =0; i<data.length;i++){
             data[i].isActive ? list[i].classList.add('active-list') :  list[i].classList.add('inactive-list')
+            data[i].isActive ? list[i].classList.remove('inactive-list') :  list[i].classList.remove('active-list')
           }
         })
 
+        document.getElementById('inactive').addEventListener('click', () =>{
+          const list = document.getElementsByClassName('list')
+          document.getElementById('inactive').classList.add('red-color')
+          document.getElementById('all').classList.remove('red-color')
+          document.getElementById('active').classList.remove('red-color')
+          for(let i= 0; i< data.length; i++){
+            data[i].isActive ? list[i].classList.remove('active-list') : list[i].classList.remove('inactive-list')
+            data[i].isActive ? list[i].classList.add('inactive-list') : list[i].classList.add('active-list')
+          }
+        })
+
+        document.getElementById('all').addEventListener('click', () =>{
+          const list = document.getElementsByClassName('list')
+          document.getElementById('all').classList.add('red-color')
+          document.getElementById('inactive').classList.remove('red-color')
+          document.getElementById('active').classList.remove('red-color')
+          for(let i=0;i<list.length;i++){
+            list[i].classList.add('active-list')
+            list[i].classList.remove('inactive-list')
+          }
+        })
     })
 
